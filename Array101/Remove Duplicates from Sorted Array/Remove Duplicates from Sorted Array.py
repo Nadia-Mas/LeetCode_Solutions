a0aee9c7f004e978401e1a7017153ffb6dc549e9
Nadia-Mas <fatemehmasoumi27@gmail.com>
2023-05-05 23:21:15 +0430

    add test

--- Array101/Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.py
+++ Array101/Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.py
@@ -1,6 +1,7 @@
 class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
 
+    def removeDuplicates(self, nums: list[int]) -> int:
+        
         unique_count = 1
         for i in range(1, len(nums)):
             if nums[i-1] != nums[i]:
