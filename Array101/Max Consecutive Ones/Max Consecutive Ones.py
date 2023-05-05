2436c960369ffd89e08732d35337c26910af253c
Nadia-Mas <fatemehmasoumi27@gmail.com>
2023-05-05 23:14:13 +0430

    add 4 problems and solutions

---
+++ Array101/Max Consecutive Ones/Max Consecutive Ones.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+
+
+        ###Solution one###
+        #         countGlobal=0
+        #         countCurrent=0
+
+        #         for i in range (len(nums)):
+
+        #             if nums[i]==1:
+        #                 countCurrent +=1
+        #                 countGlobal=max(countGlobal, countCurrent)
+
+        #             else:
+
+        #                 countCurrent=0
+
+        #         return countGlobal
+
+        #        return len(max("".join(map(str, nums)).split("0")))
+
+
+        ###Solution Two###
+        c = 0
+        a = []
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                c += 1
+
+            else:
+                a.append(c)
+                c = 0
+        a.append(c)
+        return max(a)
