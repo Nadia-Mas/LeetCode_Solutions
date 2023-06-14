769073dfab873599be471960833f01f69c562187
Nadia-Mas <fatemehmasoumi27@gmail.com>
2023-06-14 08:45:19 +0330

    push 1

--- Array101/1089.DupliacteZeros/DuplicateZeros.py
+++ Array101/1089.DupliacteZeros/DuplicateZeros.py
@@ -20,3 +20,13 @@ class Solution:
             arr.pop()
             
         return arr
+    #Approach 02
+        # i = 0
+        # while i<len(arr):
+        #     if arr[i]==0:
+        #         arr.insert(i+1, 0)
+        #         arr.pop()
+        #         i +=1
+        #     i +=1
+            
+        # return arr
