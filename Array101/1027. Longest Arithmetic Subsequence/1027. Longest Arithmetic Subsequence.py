e81b2d07616c178a3f0728ded29f93307f4f91d4
Nadia-Mas <fatemehmasoumi27@gmail.com>
2023-06-23 08:26:21 +0330

    push 215

--- Array101/1027. Longest Arithmetic Subsequence/1027. Longest Arithmetic Subsequence.py
+++ Array101/1027. Longest Arithmetic Subsequence/1027. Longest Arithmetic Subsequence.py
@@ -1,19 +1,30 @@
 class Solution:
     def longestArithSeqLength(self, nums: list[int]) -> int:
-        n=len(nums)
-        dp=[{} for _ in range(n)]
-        ans=0
-        for i in range(n):
-            dp[i][0]=1
-            for j in range(i):
-                diff=nums[i]-nums[j]
+        #Approach 01
+        # n=len(nums)
+        # dp=[{} for _ in range(n)]
+        # ans=0
+        # for i in range(n):
+        #     dp[i][0]=1
+        #     for j in range(i):
+        #         diff=nums[i]-nums[j]
 
-                if diff not in dp[j]:
-                    dp[i][diff]=2
+        #         if diff not in dp[j]:
+        #             dp[i][diff]=2
 
-                else:
-                    dp[i][diff]=dp[j][diff]+1
+        #         else:
+        #             dp[i][diff]=dp[j][diff]+1
 
-            ans=max(ans,max(dp[i].values()))
+        #     ans=max(ans,max(dp[i].values()))
 
         return ans 
+        #Approach 02
+        seq = []
+ 
+        for num in nums:
+            d = collections.defaultdict(int)
+            for i in range(len(seq)):
+                d[nums[i]-num] = seq[i][nums[i]-num] + 1
+            seq.append(d)
+        
+        return max(max(d.values()) for d in seq) + 1
